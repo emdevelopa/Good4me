5356ec98bbe58c587afec20b8dc3b65dfa20e3ee
emdevelopa <gyimahsunday@gmail.com>
2023-09-22 13:04:32 +0100

    Nice code

--- src/app/components/dealsectiong.jsx
+++ src/app/components/dealsectiong.jsx
@@ -4,7 +4,7 @@ export default function Good4meDeal() {
     return (
         <>
             <section className="py-8">
-                <div className="flex flex-col items-center gap-3">
+                <div className="flex flex-col items-center gap-3 pt-[4em]">
                     <h1 className="text-[32px] font-bold">GOOD4ME DEAL</h1>
                     <div className="w-[8em] h-[5px] bg-[#F6623E]"></div>
                     <p className="text-center">
@@ -36,7 +36,7 @@ export default function Good4meDeal() {
                         <h3 className="font-bold text-center py-4">MULTI-VITAMIN: EVERYDAY WELLNESS</h3>
                     </div>
                 </section>
-                <section className="flex gap-8 px-[10em]">
+                <section className="flex gap-8 px-[10em] py-[4em]">
                     <div className="flex flex-col justify-center gap-3 items-center">
                         <Image src='/plane.png' alt='plane' width={100} height={0} />
                         <h1 className="font-bold">WORLDWIDE SHIPPING</h1>
@@ -54,6 +54,25 @@ export default function Good4meDeal() {
                         <h1 className="font-bold">WORLDWIDE SHIPPING</h1>
                         <p className="text-center text-[#9D9D9D]">Lorem ipsum dolor sit amet, consectetuer
                             adipiscing elit. Aenean</p>
+                    </div>
+                </section>
+
+                <section className="flex">
+                    <div className="flex flex-col w-[50%]">
+                        <h1 className="font-bold text-[30px]">TAKE CONTROL OF YOUR HEALTH</h1>
+                        <p>The Good4Me range has been formulated based on scientific & traditional evidence.</p>
+                        <p>Our vitamins are here and ready to boost your mood, immunity and overall well-being!</p>
+                        <p>Made in New Zealand from local and imported ingredients.</p>
+                        <div><button className="py-4 px-6 border-[2px] border-[#F6623E]">BROWSE OUR RANGE</button></div>
+                    </div>
+                    <div className="flex gap-4 flex-shrink-3 w-[50%]">
+                        <div className="flex flex-col justify-between">
+                            <Image src='lady1.svg' alt='plane' width={0} height={0} style={{ width: 'auto', height: 'auto' }} />
+                            <Image src='lady2.svg' alt='plane' width={0} height={0} style={{ width: 'auto', height: 'auto' }} />
+                        </div>
+                        <div>
+                            <Image src='lady3.svg' alt='plane' width={0} height={0} style={{ width: 'auto', height: 'auto' }} />
+                        </div>
                     </div>
                 </section>
             </section>
