ccfb709317deefb08c86069b95f9a85d26510102
emdevelopa <gyimahsunday@gmail.com>
2023-09-22 10:19:56 +0100

    Nice code

--- src/app/components/dealsectiong.jsx
+++ src/app/components/dealsectiong.jsx
@@ -11,29 +11,27 @@ export default function Good4meDeal(){
                     Pick your beauty products today. 50% OFF on the most popular GOOD4ME. Order all classy <br/> products today!
                     </p>
                 </div>
-                <section  className=" flex justify-around py-4">
-                    <div>
-                        <div className="bg-[#E8E8E8] h-[100%] flex items-center justify-center px-2 py-[20%]">
+                <section  className=" flex gap-4 justify-around p-4">
+                    <div className="w-full">
+                        <div className="bg-[#E8E8E8] h-[90%] flex items-center justify-center px-2 py-[20%]">
                             <Image src='multivitamin.svg' alt='cards' width={200} height={0}/>
                         </div>
-                        <h3 className="font-bold ">MULTI-VITAMIN: EVERYDAY WELLNESS</h3>
+                        <h3 className="font-bold text-center">MULTI-VITAMIN: EVERYDAY WELLNESS</h3>
                     </div>
 
-                    <div className="">
-                        <div className="bg-[#E8E8E8] w-[100%] h-[100%]" style={{background:'url(multivitamin2.svg)',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
-                            {/* <Image src='multivitamin2.svg' alt='cards' width={200} height={200}/> */}
+                    <div className="w-full">
+                        <div className="bg-[#E8E8E8] w-[100%] h-[90%]" style={{background:'url(multivitamin2.svg)',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
                         </div>
-                        <h3 className="font-bold ">MULTI-VITAMIN: EVERYDAY WELLNESS</h3>
+                        <h3 className="font-bold text-center">FULL RANGE VALUE PACK (SAVE 33%)</h3>
                     </div>
-                    <div className="">
-                        <div className="bg-[#E8E8E8] h-[100%] flex items-center justify-center px-2 py-14">
+                    <div className="w-full">
+                        <div className="bg-[#E8E8E8] h-[90%] flex items-center justify-center px-2 py-14">
                             <Image src='multivitamin3.svg' alt='cards' width={200} height={200}/>
                         </div>
-                        <h3 className="font-bold ">MULTI-VITAMIN: EVERYDAY WELLNESS</h3>
+                        <h3 className="font-bold text-center">VITAMIN C: IMMUNITY SUPPORT</h3>
                     </div>
-                    <div className="">
-                        <div className="bg-[#E8E8E8] w-[100%] h-[100%]" style={{background:'url(multivitamin4.svg)',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
-                            {/* <Image src='multivitamin2.svg' alt='cards' width={200} height={200}/> */}
+                    <div className="w-full text-center">
+                        <div className="bg-[#E8E8E8] w-[100%] h-[90%]" style={{background:'url(multivitamin4.svg)',backgroundRepeat:'no-repeat',backgroundSize:'cover'}}>
                         </div>
                         <h3 className="font-bold ">MULTI-VITAMIN: EVERYDAY WELLNESS</h3>
                     </div>
